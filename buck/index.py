254c5822f70f61e856d5e4b71c494b2a3ecfef0e
Lajos Santa <santa.lajos@gmail.com>
2021-01-19 17:32:04 +0100

    enh: use pkg version as bundlejs version

--- buck/index.py
+++ buck/index.py
@@ -1,5 +1,6 @@
 import os
 from time import time
+from importlib.metadata import version
 
 from starlette.requests import Request
 from starlette.templating import Jinja2Templates
@@ -13,13 +14,15 @@ templates = Jinja2Templates(Folders.templates)
 
 async def index_endpoint(request: Request):
     context: RequestContext = request.scope[Keys.HTTP_REQUEST_CONTEXT]
+    is_dev_mode = bool(os.environ.get(Keys.DEV_MODE, 0))
+    version_str = time() if is_dev_mode else version("buck")
 
     return templates.TemplateResponse(
         name = "index.jinja2",
         context = {
             "request": request,
-            "is_dev": bool(os.environ.get(Keys.DEV_MODE, 0)),
-            "bundle_version": time(),
+            "is_dev": is_dev_mode,
+            "bundle_version": version_str,
             "script_lib_files": [],
             "claude_api_url": context.config.claude_api_url,
         }
